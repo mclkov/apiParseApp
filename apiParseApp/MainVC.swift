fe7f69cddeb94f3b4bf407323aaced5dab24a154
McL <mcl@brm>
2019-02-27 19:15:43 +0200

    UI programmatically

--- apiParseApp/MainVC.swift
+++ apiParseApp/MainVC.swift
@@ -8,11 +8,13 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .green
     }
 
 
