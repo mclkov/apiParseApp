1e89ca8def98429567d0fabf7b1728d09dc79f94
McL <mcl@brm>
2019-02-28 21:15:01 +0200

    calculator is going to be another vc

---
+++ apiParseApp/Controller/CalculatorVC.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorVC.swift
+//  apiParseApp
+//
+//  Created by McL on 2/28/19.
+//  Copyright © 2019 McL. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorVC: CustomViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
