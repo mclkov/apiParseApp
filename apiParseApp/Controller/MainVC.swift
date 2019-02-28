1a1a1d6971cf3b8a1a804bbf019afce3ff13ed8d
McL <mcl@brm>
2019-02-28 06:47:31 +0200

    MainLayout

--- apiParseApp/Controller/MainVC.swift
+++ apiParseApp/Controller/MainVC.swift
@@ -14,7 +14,7 @@ class MainVC: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        view.backgroundColor = .green
+        self.setupView()
         fetchApiData()
     }
     
---
+++ apiParseApp/View/MainLayout.swift
@@ -0,0 +1,19 @@
+//
+//  MainLayout.swift
+//  apiParseApp
+//
+//  Created by McL on 2/28/19.
+//  Copyright © 2019 McL. All rights reserved.
+//
+
+import UIKit
+
+extension MainVC {
+    func setupView() {
+        setupBackgroundColor()
+    }
+    
+    func setupBackgroundColor() {
+        view.backgroundColor = .green
+    }
+}
