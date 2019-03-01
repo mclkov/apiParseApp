318fb66ded68661c790e64ef84d1b761e9b08b1b
McL <mcl@brm>
2019-03-01 12:51:23 +0200

    exchange service

---
+++ apiParseApp/Services/ExchangeService.swift
@@ -0,0 +1,18 @@
+//
+//  CalculatorService.swift
+//  apiParseApp
+//
+//  Created by McL on 3/1/19.
+//  Copyright © 2019 McL. All rights reserved.
+//
+
+import Foundation
+
+class ExchangeService {
+    static let shared = ExchangeService()
+    
+    
+    func calculate() {
+        
+    }
+}
