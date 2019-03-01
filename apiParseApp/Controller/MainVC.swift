6a42a36d6a957e9a3ae917bf9f5eb90caaa31b41
McL <mcl@brm>
2019-03-01 14:00:14 +0200

    increase view height when keyboardWillShow

--- apiParseApp/Controller/MainVC.swift
+++ apiParseApp/Controller/MainVC.swift
@@ -31,8 +31,26 @@ class MainVC: CustomViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+
+        
         self.setupView()
         fetchLocalStorageData()
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+            if self.view.frame.origin.y == 0 {
+                self.view.frame.origin.y -= keyboardSize.height
+            }
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0 {
+            self.view.frame.origin.y = 0
+        }
     }
 
     func fetchLocalStorageData() {
